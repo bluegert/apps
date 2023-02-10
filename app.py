7c272458add000c3efca5f4a4ff59ccece12301c
bluegert <87154113+bluegert@users.noreply.github.com>
2023-02-10 23:01:06 +0100

    aslasds

--- app.py
+++ app.py
@@ -57,8 +57,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 
 if uploaded_file is not None:
     if uploaded_file.name.endswith(".pdf"):
-      st.write(base64_pdf = base64.b64encode(uploaded_file.read()).decode('utf-8')
-)
+      st.write(base64.b64encode(uploaded_file.read()).decode('utf-8'))
     #   with open(f"data:application/pdf;base64/{uploaded_file.name}", "rb") as f:
     #     base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     #     num_pages = base64_pdf.numPages
