57a5a015132e7b71e97ab3959a5bf913d9e14e10
bluegert <87154113+bluegert@users.noreply.github.com>
2023-02-10 23:02:03 +0100

    aslasds

--- app.py
+++ app.py
@@ -57,7 +57,12 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 
 if uploaded_file is not None:
     if uploaded_file.name.endswith(".pdf"):
-      st.write(base64.b64encode(uploaded_file.read()).decode('utf-8'))
+      base64_pdf = base64.b64encode(uploaded_file.read()).decode('utf-8')
+      pdf_display = (
+        f'<embed src="data:application/pdf;base64,{base64_pdf}" '
+    'width="800" height="1000" type="application/pdf"></embed>'
+    )
+    st.markdown(pdf_display, unsafe_allow_html=True)
     #   with open(f"data:application/pdf;base64/{uploaded_file.name}", "rb") as f:
     #     base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     #     num_pages = base64_pdf.numPages
