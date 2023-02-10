65ff18d39b7da4878c6c203eb32a2e13c70b0dd8
bluegert <87154113+bluegert@users.noreply.github.com>
2023-02-10 22:12:08 +0100

    aslasds

--- app.py
+++ app.py
@@ -32,8 +32,9 @@ def get_similar_terms(text_input, text_vectors):
     similarities = []
     for i in range(len(text_vectors)):
         similarities.append(cosine_similarity(text_vectors[i], search_term_vector))
-    sorted_similarities = sorted(similarities, reverse=True)
-    return sorted_similarities[0:3]
+    sorted_texts = sorted(zip(text_vectors[i], similarities), reverse=True)[:3]
+    st.write(sorted_texts)
+    return list(zip(*sorted_texts))[0]
     # df['similarities'] = df['embedding'].apply(lambda x: cosine_similarity(x, search_term_vector))
     # sorted_by_similarity = df.sort_values("similarities", ascending=False).head(3)
     # if sorted_by_similarity.iloc[2,4] < 0.8:
