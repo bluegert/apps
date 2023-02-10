0098976275729016cb818d4c2835a75215182bc5
bluegert <87154113+bluegert@users.noreply.github.com>
2023-02-10 16:31:29 +0100

    oao

--- app.py
+++ app.py
@@ -26,9 +26,8 @@ if "visibility" not in st.session_state:
 
 def get_similar_terms(text_input, df):
     search_term_vector = get_embedding(text_input, engine="text-embedding-ada-002")
-    df['similarity'] = df['vec'].apply(lambda x: cosine_similarity(x, search_term_vector))
-    st.write(df)
-    sorted_by_similarity = df.sort_values("similarity", ascending=False).head(3)
+    df['similarities'] = df['embedding'].apply(lambda x: cosine_similarity(x, search_term_vector))
+    sorted_by_similarity = df.sort_values("similarities", ascending=False).head(3)
     if sorted_by_similarity.iloc[2,4] < 0.8:
         results = "Question is out of scope. Please try to rephrase it."
     else:
