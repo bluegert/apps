af66e9de9c91405bcf3d83229fdbaffe31a742c5
bluegert <87154113+bluegert@users.noreply.github.com>
2023-02-10 18:55:43 +0100

    aslas

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def get_similar_terms(text_input, text_vectors):
     search_term_vector = get_embedding(text_input, engine="text-embedding-ada-002")
     similarities = []
     for i in range(len(text_vectors)):
-        similarities[i] = cosine_similarity(text_vectors[i], search_term_vector)
+        similarities.append(cosine_similarity(text_vectors[i], search_term_vector))
     sorted_similarities = np.argsort(similarities)
     st.write(sorted_similarities)
     # df['similarities'] = df['embedding'].apply(lambda x: cosine_similarity(x, search_term_vector))
