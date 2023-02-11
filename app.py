4b6df3ba80af4005f32c24b010f0429277371d38
bluegert <87154113+bluegert@users.noreply.github.com>
2023-02-11 09:58:55 +0100

    aslasds

--- app.py
+++ app.py
@@ -14,6 +14,32 @@ from operator import itemgetter
 import PyPDF2
 from streamlit_chat import message
 import base64
+from gpt_index import GPTSimpleVectorIndex, download_loader
+from langchain.agents import initialize_agent, Tool
+from langchain.llms import OpenAI
+from langchain.chains.conversation.memory import ConversationBufferMemory
+
+GoogleDocsReader = download_loader('GoogleDocsReader')
+
+gdoc_ids = ['1wf-y2pd9C878Oh-FmLH7Q_BQkljdm6TQal-c1pUfrec']
+loader = GoogleDocsReader()
+documents = loader.load_data(document_ids=gdoc_ids)
+index = GPTSimpleVectorIndex(documents)
+st.write(index)
+# tools = [
+#     Tool(
+#         name="Google Doc Index",
+#         func=lambda q: index.query(q),
+#         description=f"Useful when you want answer questions about the Google Documents.",
+#     ),
+# ]
+# llm = OpenAI(temperature=0)
+# memory = ConversationBufferMemory(memory_key="chat_history")
+# agent_chain = initialize_agent(
+#     tools, llm, agent="zero-shot-react-description", memory=memory
+# )
+
+# output = agent_chain.run(input="Where did the author go to school?")
 
 openai.api_key = st.secrets['api_key']
 
@@ -106,3 +132,7 @@ if uploaded_file is not None:
 # # making it look nicer
 # # data privacy
 # # some metric of performance. Can I give the model feedback? Should I?
+# organization key or private key
+# pinecone
+# the ideal dataset over time becomes more and more specific to the user and in Q&A format
+# check if embedding already exists
