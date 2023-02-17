b5fc40e8433d4c24de35659d15f934e989d86804
bluegert <87154113+bluegert@users.noreply.github.com>
2023-02-17 15:51:44 +0000

    asa

--- app.py
+++ app.py
@@ -67,7 +67,6 @@ if pdf_files:
     texts = text_splitter.split_text(df['text'][0])
     for i in range(len(texts)):
       text_vectors.append(get_embedding(texts[i], engine="text-embedding-ada-002"))
-    st.write(text_vectors)
     question = st.text_input("Enter your questions here...")
     if question != "":
         with st.spinner("Searching. Please hold..."):
