1693e9a3fbda3147eaf1d1b1cc2bf829c1bf617c
bluegert <87154113+bluegert@users.noreply.github.com>
2023-02-17 15:33:04 +0000

    asa

--- app.py
+++ app.py
@@ -1,63 +1,138 @@
-from typing import Dict
+# from flask import Flask, request, render_template
+import openai
+from openai.embeddings_utils import get_embedding, cosine_similarity
 import pandas as pd
-import PyPDF2
+import numpy as np
+import os
+# from supabase import create_client, Client
+# import asyncio
 import streamlit as st
-from sentence_transformers import SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
-from transformers import AutoTokenizer, BertForQuestionAnswering, pipeline
-from io import BytesIO
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from operator import itemgetter
+import PyPDF2
+from streamlit_chat import message
+import base64
+from gpt_index import GPTSimpleVectorIndex, download_loader
+from langchain.agents import initialize_agent, Tool
+from langchain.llms import OpenAI
+from langchain.chains.conversation.memory import ConversationBufferMemory
 
-st.title("Ask PDF Anything")
-@st.cache(allow_output_mutation=True)
-def extract_text_from_pdfs(pdf_files):
-    # Create an empty data frame
-    df = pd.DataFrame(columns=["file", "text"])
-    # Iterate over the PDF files
-    for pdf_file in pdf_files:
-        # Open the PDF file
-        # with open(pdf_file.read(), "rb") as f:
-        with BytesIO(pdf_file.read()) as f:
-            # Create a PDF reader object
-            pdf_reader = PyPDF2.PdfReader(f)
-            # Get the number of pages in the PDF
-            num_pages = len(pdf_reader.pages)
-            # Initialize a string to store the text from the PDF
-            text = ""
-            # Iterate over all the pages
-            for page_num in range(num_pages):
-                # Get the page object
-                page = pdf_reader.pages[page_num]
-                # Extract the text from the page
-                page_text = page.extract_text()
-                # Add the page text to the overall text
-                text += page_text
-            # Add the file name and the text to the data frame
-            df = df.append({"file": pdf_file.name, "text": text}, ignore_index=True)
-    # Return the data frame
-    return df
+GoogleDocsReader = download_loader('GoogleDocsReader')
 
-@st.cache(allow_output_mutation=True)
-def get_context(df, question):
-    pass
+gdoc_ids = ['https://docs.google.com/document/d/18lNKQ7EgkAQsqp-hLIZ9MvJw4IKywHCX6GOc-DQatX0/edit?usp=sharing']
+loader = GoogleDocsReader()
+documents = loader.load_data(document_ids=gdoc_ids)
+index = GPTSimpleVectorIndex(documents)
+st.write(index)
+# tools = [
+#     Tool(
+#         name="Google Doc Index",
+#         func=lambda q: index.query(q),
+#         description=f"Useful when you want answer questions about the Google Documents.",
+#     ),
+# ]
+# llm = OpenAI(temperature=0)
+# memory = ConversationBufferMemory(memory_key="chat_history")
+# agent_chain = initialize_agent(
+#     tools, llm, agent="zero-shot-react-description", memory=memory
+# )
 
-def answer_question(pipeline, question: str, context: str) -> Dict:
-    input = {"question": question, "context": context}
-    return pipeline(input)
+# output = agent_chain.run(input="Where did the author go to school?")
 
-pdf_files = st.file_uploader(
-    "Upload pdf files", type=["pdf"], accept_multiple_files=True
+openai.api_key = st.secrets['api_key']
+
+def generate_response(prompt):
+    one_shot_prompt = '''I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer.
+    Q: What is human life expectancy in the United States?
+    A: Human life expectancy in the United States is 78 years.
+    Q: '''+prompt+'''
+    A: '''
+    completions = openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt = one_shot_prompt,
+        max_tokens = 1024,
+        n = 1,
+        stop=["Q:"],
+        temperature=0.2,
+    )
+    message = completions.choices[0].text
+    return message
+
+# Store the initial value of widgets in session state
+if "visibility" not in st.session_state:
+    st.session_state.visibility = "visible"
+    st.session_state.disabled = False
+
+def get_similar_terms(text_input, text_vectors, texts):
+    search_term_vector = get_embedding(text_input, engine="text-embedding-ada-002")
+    similarities = []
+    for i in range(len(text_vectors)):
+        similarities.append(cosine_similarity(text_vectors[i], search_term_vector))
+    sorted_texts = sorted(list(zip(texts, similarities)),key=itemgetter(1), reverse=True)
+    return list(zip(*sorted_texts[:3]))
+
+text_splitter = CharacterTextSplitter(        
+    separator = "\n",
+    chunk_size = 1000,
+    chunk_overlap  = 200,
+    length_function = len,
 )
+uploaded_file = st.file_uploader("Choose a file first", type="txt")
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
-if pdf_files:
-    with st.spinner("processing pdf..."):
-        df = extract_text_from_pdfs(pdf_files)
-    question = st.text_input("Enter your questions here...")
-    st.write(df['text'])
-    # if question != "":
-    #     with st.spinner("Searching. Please hold..."):
-    #         context = get_context(df, question)
-    #         st.write(context)
-            # answer = answer_question(question, context)
-            # st.write(answer)
-    #     del qa_pipeline
-    #     del context
+if uploaded_file is not None:
+    # if uploaded_file.name.endswith(".pdf"):
+    #   base64_pdf = base64.b64encode(uploaded_file.read()).decode('utf-8')
+    #   pdf_display = (
+    #     f'<embed src="data:application/pdf;base64,{base64_pdf}" '
+    # 'width="800" height="1000" type="application/pdf"></embed>'
+    # )
+    #   st.markdown(pdf_display, unsafe_allow_html=True)
+    # else:
+    texts = text_splitter.split_text(uploaded_file.read().decode("utf-8"))
+    text_vectors = []
+    for i in range(len(texts)):
+      text_vectors.append(get_embedding(texts[i], engine="text-embedding-ada-002"))
+
+    text_input = st.text_input(
+        "Ask a question 👇", # make this custom to the pdf
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled
+        # placeholder=st.session_state.placeholder,
+    )
+
+    if text_input:
+      if 'generated' not in st.session_state:
+            st.session_state['generated'] = []
+
+      if 'past' not in st.session_state:
+          st.session_state['past'] = []
+      similar_terms = get_similar_terms(text_input, text_vectors, texts)
+      user_input_embedding_prompt = 'Using this context: "'+str(similar_terms[0])+'", answer the following question changing as little wording as possible of the context. \n'+ text_input
+      st.write(user_input_embedding_prompt)
+#       response = generate_response(user_input_embedding_prompt)
+#       # if already text generated, build on that
+#       if st.session_state['generated']:
+#         st.write(st.session_state['generated'])
+#       st.session_state.past.append(text_input)
+#       st.session_state.generated.append(response)
+#       if st.session_state['generated']:
+#         for i in range(len(st.session_state['generated'])-1):
+#             message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+#             message(st.session_state["generated"][i], key=str(i))
+
+# # pdf support
+# # https://discuss.streamlit.io/t/how-to-display-pdf-files-in-streamlit/1806/2
+# # prompts building on each other
+# # summarizer
+# # better prompts (see youtube gpt)
+# # multiple files upload
+# # making it look nicer
+# # data privacy
+# # some metric of performance. Can I give the model feedback? Should I?
+# organization key or private key
+# pinecone
+# the ideal dataset over time becomes more and more specific to the user and in Q&A format
+# check if embedding already exists
