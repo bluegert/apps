565be086ce020adeefc9a96384e69c005938ab28
bluegert <87154113+bluegert@users.noreply.github.com>
2023-02-17 15:49:05 +0000

    asa

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ if pdf_files:
     with st.spinner("processing pdf..."):
         df = extract_text_from_pdfs(pdf_files)
     text_vectors = []
-    texts = text_splitter(df['text'])
+    texts = text_splitter.split_text(df['text'])
     st.write(texts)
     # for i in range(len(texts)):
     #   text_vectors.append(get_embedding(texts[i]), engine="text-embedding-ada-002")
