52242a045712839394eeb0aaff4108c84a6eb2a2
bluegert <87154113+bluegert@users.noreply.github.com>
2023-02-17 14:22:00 +0000

    y

--- app.py
+++ app.py
@@ -1,87 +1,16 @@
-# from flask import Flask, request, render_template
-import openai
-from openai.embeddings_utils import get_embedding, cosine_similarity
+from typing import Dict
+
 import pandas as pd
-import numpy as np
-import os
-# from supabase import create_client, Client
-# import asyncio
-import streamlit as st
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from operator import itemgetter
-from streamlit_chat import message
-import base64
-from gpt_index import GPTSimpleVectorIndex, download_loader
-from langchain.agents import initialize_agent, Tool
-from langchain.llms import OpenAI
-from langchain.chains.conversation.memory import ConversationBufferMemory
-from io import BytesIO
 import PyPDF2
+import streamlit as st
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+from transformers import AutoTokenizer, BertForQuestionAnswering, pipeline
 
-# tools = [
-#     Tool(
-#         name="Google Doc Index",
-#         func=lambda q: index.query(q),
-#         description=f"Useful when you want answer questions about the Google Documents.",
-#     ),
-# ]
-# llm = OpenAI(temperature=0)
-# memory = ConversationBufferMemory(memory_key="chat_history")
-# agent_chain = initialize_agent(
-#     tools, llm, agent="zero-shot-react-description", memory=memory
-# )
+from io import BytesIO
 
-# output = agent_chain.run(input="Where did the author go to school?")
+st.title("Question-Answering Webapp")
 
-openai.api_key = st.secrets['api_key']
-
-def generate_response(prompt):
-    one_shot_prompt = '''I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer.
-    Q: What is human life expectancy in the United States?
-    A: Human life expectancy in the United States is 78 years.
-    Q: '''+prompt+'''
-    A: '''
-    completions = openai.Completion.create(
-        engine = "text-davinci-003",
-        prompt = one_shot_prompt,
-        max_tokens = 2048,
-        n = 1,
-        stop=["Q:"],
-        temperature=0.2,
-    )
-    message = completions.choices[0].text
-    return message
-
-# Store the initial value of widgets in session state
-if "visibility" not in st.session_state:
-    st.session_state.visibility = "visible"
-    st.session_state.disabled = False
-
-def get_similar_terms(text_input, text_vectors, texts):
-    search_term_vector = get_embedding(text_input, engine="text-embedding-ada-002")
-    similarities = []
-    for i in range(len(text_vectors)):
-        similarities.append(cosine_similarity(text_vectors[i], search_term_vector))
-    sorted_texts = sorted(list(zip(texts, similarities)),key=itemgetter(1), reverse=True)
-    return list(zip(*sorted_texts[:3]))
-
-def clean_text(serie):
-    serie = serie.replace('\n', ' ')
-    serie = serie.replace('\\n', ' ')
-    serie = serie.replace('  ',' ')
-    serie = serie.replace('  ',' ')
-    return serie
-
-text_splitter = CharacterTextSplitter(        
-    separator = "\n",
-    chunk_size = 1000,
-    chunk_overlap  = 200,
-    length_function = len,
-)
-uploaded_file = st.file_uploader("Choose a file first", type="pdf")
-st.set_option('deprecation.showfileUploaderEncoding', False)
 
 @st.cache(allow_output_mutation=True)
 def extract_text_from_pdfs(pdf_files):
@@ -109,57 +38,111 @@ def extract_text_from_pdfs(pdf_files):
             # Add the file name and the text to the data frame
             df = df.append({"file": pdf_file.name, "text": text}, ignore_index=True)
     # Return the data frame
-    return 
+    return df
 
-if uploaded_file is not None:
-    text = extract_text_from_pdfs(uploaded_file)
-    st.write(text)
-    text = ""
-    # for page in reader.pages:
-    #       text += page.extract_text() + "\n"
-    text = clean_text(text)
-    texts = text_splitter.split_text(text)
-    text_vectors = []
-    for i in range(len(texts)):
-      text_vectors.append(get_embedding(texts[i], engine="text-embedding-ada-002"))
 
-    text_input = st.text_input(
-        "Ask a question 👇", # make this custom to the pdf
-        label_visibility=st.session_state.visibility,
-        disabled=st.session_state.disabled
-        # placeholder=st.session_state.placeholder,
+def preprocess_text(text_list):
+    # Initialize a empty list to store the pre-processed text
+    processed_text = []
+    # Iterate over the text in the list
+    for text in text_list:
+        num_words = len(text.split(" "))
+        if num_words > 10:  # only include sentences with length >10
+            processed_text.append(text)
+    # Return the pre-processed text
+    return processed_text
+
+
+def remove_short_sentences(df):
+    df["sentences"] = df["sentences"].apply(preprocess_text)
+    return df
+
+
+@st.cache(allow_output_mutation=True)
+def get_relevant_texts(df, topic):
+    model_embedding = SentenceTransformer("all-MiniLM-L6-v2")
+    model_embedding.save("all-MiniLM-L6-v2")
+    cosine_threshold = 0.3  # set threshold for cosine similarity value
+    queries = topic  # search query
+    results = []
+    for i, document in enumerate(df["sentences"]):
+        sentence_embeddings = model_embedding.encode(document)
+        query_embedding = model_embedding.encode(queries)
+        for j, sentence_embedding in enumerate(sentence_embeddings):
+            distance = cosine_similarity(
+                sentence_embedding.reshape((1, -1)), query_embedding.reshape((1, -1))
+            )[0][0]
+            sentence = df["sentences"].iloc[i][j]
+            results += [(i, sentence, distance)]
+    results = sorted(results, key=lambda x: x[2], reverse=True)
+    del model_embedding
+
+    texts = []
+    for idx, sentence, distance in results:
+        if distance > cosine_threshold:
+            text = sentence
+            texts.append(text)
+    # turn the list to string
+    context = "".join(texts)
+    return context
+
+
+@st.cache(allow_output_mutation=True)
+def get_pipeline():
+    modelname = "deepset/bert-base-cased-squad2"
+    model_qa = BertForQuestionAnswering.from_pretrained(modelname)
+    # model_qa.save_pretrained(modelname)
+    tokenizer = AutoTokenizer.from_pretrained("tokenizer-deepset")
+    # tokenizer.save_pretrained("tokenizer-" + modelname)
+    qa = pipeline("question-answering", model=model_qa, tokenizer=tokenizer)
+    return qa
+
+
+def answer_question(pipeline, question: str, context: str) -> Dict:
+    input = {"question": question, "context": context}
+    return pipeline(input)
+
+
+@st.cache(allow_output_mutation=True)
+def create_context(df):
+    # path = "data/"
+    # files = Path(path).glob("WHR+22.pdf")
+    # df = extract_text_from_pdfs(files)
+    df["sentences"] = df["text"].apply(
+        lambda long_str: long_str.replace("\n", " ").split(".")
     )
+    df = remove_short_sentences(df)
 
-    if text_input:
-      if 'generated' not in st.session_state:
-            st.session_state['generated'] = []
+    context = get_relevant_texts(df, topic)
+    return context
 
-      if 'past' not in st.session_state:
-          st.session_state['past'] = []
-      similar_terms = get_similar_terms(text_input, text_vectors, texts)
-      user_input_embedding_prompt = 'Using this context: "'+str(similar_terms[0])+'", answer the following question changing staying entirely true to the context:'+ text_input
-      st.write(user_input_embedding_prompt)
-      response = generate_response(user_input_embedding_prompt)
-      # if already text generated, build on that
-      if st.session_state['generated']:
-        st.write(st.session_state['generated'])
-      st.session_state.past.append(text_input)
-      st.session_state.generated.append(response)
-      if st.session_state['generated']:
-        for i in range(len(st.session_state['generated'])-1):
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
 
-# # pdf support
-# # https://discuss.streamlit.io/t/how-to-display-pdf-files-in-streamlit/1806/2
-# # prompts building on each other
-# # summarizer
-# # better prompts (see youtube gpt)
-# # multiple files upload
-# # making it look nicer
-# # data privacy
-# # some metric of performance. Can I give the model feedback? Should I?
-# organization key or private key
-# pinecone
-# the ideal dataset over time becomes more and more specific to the user and in Q&A format
-# check if embedding already exists
+@st.cache(allow_output_mutation=True)
+def start_app():
+    with st.spinner("Loading model. Please hold..."):
+        # context = create_context()
+        pipeline = get_pipeline()
+    return pipeline
+
+
+pdf_files = st.file_uploader(
+    "Upload pdf files", type=["pdf"], accept_multiple_files=True
+)
+
+if pdf_files:
+    with st.spinner("processing pdf..."):
+        df = extract_text_from_pdfs(pdf_files)
+        # context = create_context(df)
+        # del df
+    topic = st.text_input("Enter the topic you want to ask here")
+    question = st.text_input("Enter your questions here...")
+
+    if question != "":
+        # pipeline = get_pipeline()
+        with st.spinner("Searching. Please hold..."):
+            context = create_context(df)
+            qa_pipeline = start_app()
+            answer = answer_question(qa_pipeline, question, context)
+            st.write(answer)
+        del qa_pipeline
+        del context
