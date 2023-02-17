baeaa73c8da249697a7a57ea8e82825cacc84fc3
bluegert <87154113+bluegert@users.noreply.github.com>
2023-02-17 12:53:04 +0000

    yes

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ if uploaded_file is not None:
     # )
     #   st.markdown(pdf_display, unsafe_allow_html=True)
     # else:
-    reader = PdfReader(file=uploaded_file)
+    reader = PdfReader(file=uploaded_file.read().decode('utf-8'))
     text = ""
     for page in reader.pages:
           text += page.extract_text() + "\n"
