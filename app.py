de74f8617db716377e7531022b4e804ee30c278b
bluegert <87154113+bluegert@users.noreply.github.com>
2023-02-17 15:55:16 +0000

    asa

--- app.py
+++ app.py
@@ -64,14 +64,16 @@ if pdf_files:
     with st.spinner("processing pdf..."):
         df = extract_text_from_pdfs(pdf_files)
     text_vectors = []
-    texts = text_splitter.split_text(df['text'][0])
+    texts = text_splitter.split_text(df['text'])
+    st.write(texts)
     for i in range(len(texts)):
       text_vectors.append(get_embedding(texts[i], engine="text-embedding-ada-002"))
+    
     question = st.text_input("Enter your questions here...")
     if question != "":
         with st.spinner("Searching. Please hold..."):
-            context = get_context(question, text_vectors, df['text'][0])
-            st.write(context)
+            context = get_context(question, text_vectors, df['text'])
+            # st.write(context)
             # answer = answer_question(question, context)
             # st.write(answer)
     #     del qa_pipeline
